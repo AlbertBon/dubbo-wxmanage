c9c71eed28710f62dabf0859257f361ac101f4d0
pengwen <502285815@qq.com>
2018-04-29 16:06:46 +0800

    web不加载数据源（更换扫描包，不加载数据源自动配置）

--- dubbo-web/src/main/java/com/bon/DubboWebApplication.java
+++ dubbo-web/src/main/java/com/bon/DubboWebApplication.java
@@ -1,7 +1,6 @@
 package com.bon;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,9 +12,8 @@ import org.springframework.context.annotation.ComponentScan;
  * @author pengwen
  * @create 2018/4/28 0028
  **/
-@SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-@ComponentScan({"com.bon"})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@ComponentScan({"com.bon.api"})
 public class DubboWebApplication {
 
     public static void main(String[] args) {
