dbdebdc53776dcc40e2d8506e1cbf2a335e55dc0
officeBon <officeBon@qq.com>
2018-05-07 11:20:49 +0800

    mybatis覆盖XML文件

---
+++ common/src/main/java/com/bon/common/plugins/OverIsMergeablePlugin.java
@@ -0,0 +1,33 @@
+package com.bon.common.plugins;
+
+import org.mybatis.generator.api.GeneratedXmlFile;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+/**
+ * @program: dubbo-wxmanage
+ * @description: mybatis生成xml覆盖插件
+ * @author: Bon
+ * @create: 2018-05-07 11:16
+ **/
+public class OverIsMergeablePlugin extends PluginAdapter {
+    @Override
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+    @Override
+    public boolean sqlMapGenerated(GeneratedXmlFile sqlMap, IntrospectedTable introspectedTable) {
+        try {
+            Field field = sqlMap.getClass().getDeclaredField("isMergeable");
+            field.setAccessible(true);
+            field.setBoolean(sqlMap, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+}
